342b39eae55e39c111b32ec83661d351100aefa5
shuanand@itron.com <Shubham.Anand@itron.com>
2023-07-02 02:06:05 +0530

    added the table in the DB

---
+++ Identity.Reconcilation/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Reconcilation.Models
+{
+    public class Contact
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int? LinkedId { get; set; }
+        public string LinkPrecedence { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ Identity.Reconcilation/Models/ContactContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Reconcilation.Models
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions <ContactContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
